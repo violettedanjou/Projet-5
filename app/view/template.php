c2558a4476663499112e044cb38099d883153f9b
YOYO8 <violette.danjou@hotmail.fr>
2021-03-01 12:13:17 +0100

    header ok dans template

--- app/view/template.php
+++ app/view/template.php
@@ -7,10 +7,49 @@
 
 	<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css"> <!-- Icones -->
 
-	<link href="public/style.css" rel="stylesheet" /> 
+	<link href="app/public/style.css" rel="stylesheet" /> 
 	<title><?= $title ?></title>
 </head>
 <body>
+    <div class="wrap">
+        <header>
+		    	<div id="caledonia">
+		    		<a href="index.php">
+		    			<img src="app/public/pictures/logo.png" alt="Logo de l'emblème de la Nlle-Calédonie">
+						La Calédonienne <br/>
+						<span>Office de tourisme <br/> (Fictif) </span>
+					</a>
+				</div>
+					
+				<nav>
+					<ul>
+						<?php if(isset($_SESSION['pseudo'])) { ?>	
+							<li class="li" id="button_deconnexion"><a href="index.php?action=validDeconnexion">DÉCONNEXION</a></li>
+							<li class="li" id="button_profile"><a href="index.php?action=validProfile">COMPTE</a></li>
 
+							<?php if($_SESSION['admin'] != 0) { ?>
+								<li class="li" id="button_activities"><a href="index.php?action=adminActivites">ACTIVITÉS</a></li>
+								<li class="li" id="button_hotels"><a href="index.php?action=adminHotels">HÔTELS</a></li>
+								<li class="li" id="button_opinions"><a href="index.php?action=adminOpinions">AVIS</a></li>
+
+								<!-- <li class="li" id="button_admin"><a href="index.php?action=afficheAdmin">AVIS</a></li>-->
+							<?php }
+						}
+						
+						else { 
+						?> 
+							<li class="li" id="button_signup"><a href="index.php?action=afficheSignup">CRÉER UN COMPTE</a></li>
+							<li class="li" id="button_signin"><a href="index.php?action=afficheSignin">CONNEXION</a></li>
+						<?php }?>
+					</ul>
+				</nav>
+		</header>
+			
+	   	<?= $content ?>
+    </div>
+
+	<footer>
+    	<p> Copyright © Violette Danjou - 2021. Tous droits réservés</p>
+    </footer>
 </body>
 </html>
