3b3554bd555d094b2f291cb00c54ca9e81e675d0
YOYO8 <violette.danjou@hotmail.fr>
2021-03-01 11:48:10 +0100

    début projet

--- app/view/template.php
+++ app/view/template.php
@@ -1,8 +1,9 @@
 <!DOCTYPE html>
 <html>
 <head>
+	<meta charset="utf-8" />
 	<link rel= "preconnect" href="https://fonts.gstatic.com">
-	<link href="https://fonts.googleapis.com/css2?family=Quicksand&display=swap" rel="feuille de style">
+	<link href="https://fonts.googleapis.com/css2?family=Quicksand&display=swap" rel="stylesheet">
 
 	<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css"> <!-- Icones -->
 
