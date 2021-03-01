ee4cb2cc265fff8aac3277f908d5da4c34b56b91
YOYO8 <violette.danjou@hotmail.fr>
2021-03-01 13:58:05 +0100

    Connexion à la base de données dans Manager.php

---
+++ app/model/Manager.php
@@ -0,0 +1,9 @@
+<?php
+class Manager
+{
+    protected function dbConnect()
+    {
+        $db = new PDO('mysql:host=localhost;dbname=projet-5;charset=utf8', 'root', 'root', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+        return $db;
+    }
+}
