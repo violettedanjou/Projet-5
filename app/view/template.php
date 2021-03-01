232845ebd35bb980df2d1b471a7acdf6c77ac21b
YOYO8 <violette.danjou@hotmail.fr>
2021-03-01 11:52:25 +0100

    head ok

--- app/view/template.php
+++ app/view/template.php
@@ -3,7 +3,7 @@
 <head>
 	<meta charset="utf-8" />
 	<link rel= "preconnect" href="https://fonts.gstatic.com">
-	<link href="https://fonts.googleapis.com/css2?family=Quicksand&display=swap" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css2?family=Quicksand&display=swap" rel="stylesheet"> <!-- Police -->
 
 	<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css"> <!-- Icones -->
 
