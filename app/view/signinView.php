f75821a709ab85f12b4a7dc41ec209b484f3946d
YOYO8 <violette.danjou@hotmail.fr>
2021-03-04 16:25:42 +0100

    CSS sur les pages dinscription et de connexion

--- app/view/signinView.php
+++ app/view/signinView.php
@@ -2,19 +2,20 @@
 
 ob_start(); ?>
 
-<div class="container">
-	<div class="row form-connexion">
-		<div class="col-12">
-			<h1>SE CONNECTER</h1>
-				<form action="index.php?action=validSignin" method="post">
-				    <label for="pseudo">Nom / Pseudo </label><input type="text" name="pseudo" required class="input-signin" /><br/>
-				    <label for="pass">Mot de passe </label><input type="password" name="pass" required class="input-signin"/><br/>
-				    <input type="submit" name="signin" value="SE CONNECTER" id="button_signin">
-				</form>
+<body id="section-signup"> 
+	<div class="container">
+		<div class="row form-connexion">
+			<div class="col">
+				<h2>SE CONNECTER</h2>
+					<form action="index.php?action=validSignin" method="post">
+					    <label for="pseudo">Nom / Pseudo </label><input type="text" name="pseudo" required class="input-signin" /><br/>
+					    <label for="pass">Mot de passe </label><input type="password" name="pass" required class="input-signin"/><br/>
+					    <input type="submit" name="signin" value="SE CONNECTER" id="button_confirm_signin">
+					</form>
+				</div>
 			</div>
-		</div>
-</div>
-
+	</div>
+</body>
 <?php $content = ob_get_clean(); 
 require('app/view/template.php');
 ?>
--- app/view/signupView.php
+++ app/view/signupView.php
@@ -2,20 +2,22 @@
 
 ob_start(); ?>
 
-<div class="container">
-	<div class="row signup-form">
-		<div class="col-12">
-			<h1>S'INSCRIRE</h1>
-				<form action="index.php?action=validSignup" method="post">
-					<label for="pseudo">Nom / Pseudo </label><input type="text" name="pseudo" class="input-signup" /><br/>
-					<label for="pass">Mot de passe </label><input type="password" name="pass" class="input-signup"/><br/>
-					<label for="pass_confirm">Confirmation du mot de passe </label><input type="password" name="pass_confirm" class="input-signup"/><br/>
-					<label for="email">Adresse email :</label><input type="email" name="email"  class="input-signup"/><br/>
-					<input type="submit" name="signup" value="S'INSCRIRE" id="button_signup">
-				</form>
+<body id="section-signup">
+	<div class="container">
+		<div class="row signup-form">
+			<div class="col">
+				<h2>S'INSCRIRE</h2>
+					<form action="index.php?action=validSignup" method="post">
+						<label for="pseudo">Nom / Pseudo </label><input type="text" name="pseudo" class="input-signup" /><br/>
+						<label for="pass">Mot de passe </label><input type="password" name="pass" class="input-signup"/><br/>
+						<label for="pass_confirm">Confirmation du mot de passe </label><input type="password" name="pass_confirm" class="input-signup"/><br/>
+						<label for="email">Adresse email </label><input type="email" name="email"  class="input-signup" style="width: 250px"/><br/>
+						<input type="submit" name="signup" value="S'INSCRIRE" id="button_confirm_signup">
+					</form>
+				</div>
 			</div>
-		</div>
-</div>
+	</div>
+</body>
 
 <?php $content = ob_get_clean(); 
 require('app/view/template.php'); ?>
