1d148e2fbfe26ebcd130ab5255c77f764916b8a3
YOYO8 <violette.danjou@hotmail.fr>
2021-03-05 15:13:02 +0100

    Création de la fonction pseudoAuthor()

---
+++ app/model/OpinionsManager.php
@@ -0,0 +1,20 @@
+<?php
+namespace app\model\OpinionsManager;
+
+require "vendor/autoload.php";
+use app\model\OpinionsManager;
+
+class OpinionsManager extends Manager 
+{
+    public function pseudoAuthor($activityId) // Jointure : récupérer le pseudo d'un membre grace à son id
+    {
+    	$db = $this->dbConnect();
+    	$pseudoOpinion = $db->prepare('SELECT members.pseudo, opinions.id, opinions.content, DATE_FORMAT(opinions.date, \'%d/%m/%Y à %Hh%imin%ss\') AS opinion_date_fr FROM opinions INNER JOIN members ON opinions.author = members.id WHERE opinions.opinion_id = ?');
+    	$pseudoOpinion->execute(array($activityId));
+
+    	return $pseudoOpinion;
+    }	
+}
+
+
+?>
