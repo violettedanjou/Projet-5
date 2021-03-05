148f60561706a17864dc6fab892f4b802abd222a
YOYO8 <violette.danjou@hotmail.fr>
2021-03-05 12:05:37 +0100

    Modifications du texte de la legende

--- app/view/homeView.php
+++ app/view/homeView.php
@@ -8,9 +8,9 @@
     <div class="container">
         <div class="row">
             <div class="col">
-                <p class="p-legend">Texte pour histoire de la nouvelle Calédonie : La Nouvelle-Calédonie, île française située dans le pacifique à plus de 17 000km de Paris offrant un cadre paradisiaque avec le plus grand lagon du monde inscrit au patrimoine mondiale de l’UNESCO. Cet archipel regorge de plages de sable blanc et d‘animaux en tout genre avec l’ensemble de ses cétacés comprenant baleines, dauphins mais aussi raie manta, tortues et bien d’autres encore… Terre de partage, les différentes cultures de la nouvelle Calédonie font partie intégrante de l’ile avec notamment avec la culture kanak et ses nombreuses coutumes et traditions. Un véritable voyage vous attends au bout du monde… </p>
+                <p class="p-legend">La Nouvelle-Calédonie, île française située dans le pacifique à plus de 17 000km de Paris offre un cadre paradisiaque avec le plus grand lagon du monde inscrit au patrimoine mondiale de l’UNESCO. Cet archipel regorge de plages de sable blanc et d‘animaux en tout genre avec l’ensemble de ses cétacés comprenant baleines, dauphins mais aussi raie manta, tortues et bien d’autres encore… Terre de partage, les différentes cultures de la nouvelle Calédonie font partie intégrante de l’ile avec notamment avec la culture kanak et ses nombreuses coutumes et traditions. Un véritable voyage vous attends au bout du monde… </p>
 
-                <p class="p-legend">Retrouvez sur notre site l’ensemble des activités à réaliser sur la Grande Terre ou sur les îles Loyauté. De nombreuses activités vous attendent pour vivre pleinement votre voyage entre activités sportives et culturelles comme kitesurf, les balades à cheval, les musées ou encore le shopping au centre de la capitale Nouméa.</p>
+                <p class="p-legend">Retrouvez sur notre site l’ensemble des activités à réaliser sur la Grande Terre ou sur les îles Loyauté. De nombreuses activités vous attendent pour vivre pleinement votre voyage entre activités sportives et culturelles comme kitesurf, les balades à cheval, les musées ou encore le shopping au centre de la capitale de Nouméa.</p>
             </div>
         </div>
     </div>
