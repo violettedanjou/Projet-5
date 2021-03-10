34d6d84c34a70b8ea0b22116bd8489fdf2611953
YOYO8 <violette.danjou@hotmail.fr>
2021-03-10 16:29:13 +0100

    Mise en forme page des erreurs (mais pb avec le footer non résolu) + reglages du footer avec la classe wrap

--- app/view/errorView.php
+++ app/view/errorView.php
@@ -2,17 +2,26 @@
 
 ob_start(); ?>
 
-<h1>Erreur</h1>
+<body id="body-error">
+	<h1>Erreur</h1>
 
-<div id="div-error">
-	<?php 
-	echo "" . $e->getMessage();
-	?> 
-	<br/>
-	<a href="index.php" class="btn_return_index">
-		<i class="fas fa-arrow-left"></i>Retour
-	</a>
-</div>
+	<div class="container">
+		<div class="row error">
+			<div class="col">
+				<div>
+					<?php 
+					echo "" . $e->getMessage();
+					?> 
+					<br/>
+					<a href="index.php" class="btn_return_index">
+						<i class="fas fa-arrow-left"></i>Retour
+					</a>
+				</div>
+			</div>
+		</div>
+	</div>
+</body>
+
 
 <?php 
 $content = ob_get_clean(); 
