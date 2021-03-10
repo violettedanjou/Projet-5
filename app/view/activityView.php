e33b6c19584fd409107b4ebc643639ff72248e7b
YOYO8 <violette.danjou@hotmail.fr>
2021-03-10 10:38:02 +0100

    Rectification du nom de laction pour retourner sur la page admin

--- app/view/activityView.php
+++ app/view/activityView.php
@@ -9,7 +9,7 @@ ob_start(); ?>
 
 			<p> <!-- Lien de retour vers liste des activités -->
 			<?php 	if(isset($_SESSION['admin']) && ($_SESSION['admin'] != 0)) { ?>
-						<a href="index.php?action=afficheAdmin">
+						<a href="index.php?action=openAdmin">
 							<i class="fas fa-arrow-left"></i>Retour
 						</a>
 			<?php 	}
