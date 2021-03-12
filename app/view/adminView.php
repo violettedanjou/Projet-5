fc7de6bc2243590b8cf34a8081c70415a34eba3a
YOYO8 <violette.danjou@hotmail.fr>
2021-03-12 17:56:25 +0100

    Ajout du bloc de la liste des hotels dans partie admin

--- app/view/adminView.php
+++ app/view/adminView.php
@@ -41,6 +41,41 @@ ob_start(); ?>
 
 
 <h1>GESTION DES HOTELS</h1>
+<div class="container">
+	<div class="row admin">
+		<div class="col">
+			<a class="link-add" href="index.php?action=openNewHotel">Ajouter</a><br/>
+				<br/>
+			<?php
+			while ($data = $hotels->fetch()) 
+			{
+			?>
+			    <div class="news-admin">
+			    	<a class="link-edit" href="index.php?action=openChange&amp;id=<?= $data['id'] ?>">Modifier</a>
+
+			    	<div class="news-img-text">
+			    		<img src="<?= $data['picture'] ?>" alt="Hotel <?= $hotel['name'] ?>">
+
+				    	<div class="news-text-admin">
+				    		<h3>
+					        	<a href="index.php?action=hotel&amp;id=<?= $data['id'] ?>">
+					            	<?= htmlspecialchars($data['title']) ?>
+					            </a>
+				        	</h3>
+
+				       		<p><?= nl2br($data['content']) ?> <br/></p>
+				    	</div>
+			    	</div>
+			    	
+
+			    	<a class="link-delete" href="index.php?action=validDelete&amp;id=<?= $data['id'] ?>">Supprimer</a>
+		    	</div>
+			<?php 
+			} 
+			$hotels->closeCursor(); ?>
+		</div>
+	</div>
+</div>
 
 
 <h1>GESTION DES AVIS SIGNALES</h1>
