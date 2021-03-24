8ec395a329476d5f7f20e26c7e0e8b4e8c5ead62
YOYO8 <violette.danjou@hotmail.fr>
2021-03-24 15:17:10 +0100

    Ajouts de nouvelles conditions dans .js

--- weather.js
+++ weather.js
@@ -3,6 +3,7 @@ class Weather {
 	{
 		this.id = id;
 		this.description = description;
+
 		this.urlThundertsorm = "http://openweathermap.org/img/wn/11d.png";
 		this.urlDrizzle = "http://openweathermap.org/img/wn/09d.png";
 		this.urlRain = "http://openweathermap.org/img/wn/10d.png";
@@ -13,21 +14,11 @@ class Weather {
 			urlClearDay = "http://openweathermap.org/img/wn/01d.png", 
 			urlClearNight = "http://openweathermap.org/img/wn/01n.png"
 		});
-		
-		this.urlFewClouds = new Icon({ // quelques nuages
-			urlFewDay = "http://openweathermap.org/img/wn/02d.png", 
-			urlFewNight = "http://openweathermap.org/img/wn/02n.png"
-		});
-
-		this.urlScatteredClouds = new Icon({ // nuages éparpillés
-			urlScatteredDay = "http://openweathermap.org/img/wn/03d.png", 
-			urlScatteredNight = "http://openweathermap.org/img/wn/03n.png"
+	
+		this.urlClouds = new Icon({ // nuages éparpillés
+			urlCloudsDay = "http://openweathermap.org/img/wn/03d.png", 
+			urlCloudsNight = "http://openweathermap.org/img/wn/03n.png"
 		});		
-
-		this.urlClouds = new Icon({ // nuages brisés et nuageux
-			urlCloudsDay = "http://openweathermap.org/img/wn/04d.png", 
-			urlCloudsNight = "http://openweathermap.org/img/wn/04n.png"
-		});
 
 		/*
 		this.urlClear = "http://openweathermap.org/img/wn/01d.png"; this.urlClear = "http://openweathermap.org/img/wn/01n.png";
@@ -47,14 +38,43 @@ class Weather {
 				let description = data[i].weather[0].description;
 				let icon = data[i].weather[0].icon;
 
-				if (data[i].weather[0].id == 800) { // SI l'id est égal à 800 ALORS on affiche la description de la météo et l'icon du soleil 
+	
+				// Orages (thunderstorm)
+				if (data[i].weather[0].id >= 200 && <= 232) { // SI l'id est supérieur ou égal à 200 et inférieur ou égal à 232 ALORS on affiche la description et l'icon des orages 
+					description = this.description;
+					icon = this.urlThundertsorm;
+				}
+				// Petite pluie (drizzle)
+				else if (data[i].weather[0].id >= 300 && <= 321) { // SI l'id est supérieur ou égal à 300 et inférieur ou égal à 321 ALORS on affiche la description et l'icon de la bruine 
+					description = this.description;
+					icon = this.urlDrizzle;
+				}
+				// Pluie (rain)
+				else if (data[i].weather[0].id >= 500 && <= 531) { // SI l'id est supérieur ou égal à 500 et inférieur ou égal à 531 ALORS on affiche la description et l'icon de la pluie 
+					description = this.description;
+					icon = this.urlRain;
+				}
+				// Neige (snow)
+				else if (data[i].weather[0].id >= 600 && <= 622) { // SI l'id est supérieur ou égal à 600 et inférieur ou égal à 622 ALORS on affiche la description et l'icon de la neige 
+					description = this.description;
+					icon = this.urlSnow;
+				}								
+				// Brouillard (atmosphere)
+				else if (data[i].weather[0].id >= 701 && <= 781) { // SI l'id est supérieur ou égal à 701 et inférieur ou égal à 781 ALORS on affiche la description et l'icon du brouillard 
+					description = this.description;
+					icon = this.urlAtmosphere;
+				}								
+				// Sun (clear)
+				else if (data[i].weather[0].id == 800) { // SI l'id est égal à 800 ALORS on affiche la description de la météo et l'icon du soleil 
 					description = this.description;
 					icon = this.urlClear;
 				}
-				if (data[i].weather[0].id === 801) { // SI l'id est supérieur à 800 ALORS on affiche la description et l'icon des nuages 
+				// Nuages (clouds)
+				else if (data[i].weather[0].id >= 801) { // SI l'id est supérieur ou égal à 801 ALORS on affiche la description et l'icon des nuages 
 					description = this.description;
-					icon = this.urlFewClouds;
-				}
+					icon = this.urlClouds;
+				} 
+							
 
 
 				// var iconurl = "http://openweathermap.org/img/w/" + iconcode + ".png";
@@ -62,3 +82,26 @@ class Weather {
 		}
 	}
 }
+
+
+
+
+
+/* 		this.urlFewClouds = new Icon({ // quelques nuages
+			urlFewDay = "http://openweathermap.org/img/wn/02d.png", 
+			urlFewNight = "http://openweathermap.org/img/wn/02n.png"
+		});
+		this.urlClouds = new Icon({ // nuages brisés et nuageux
+			urlCloudsDay = "http://openweathermap.org/img/wn/04d.png", 
+			urlCloudsNight = "http://openweathermap.org/img/wn/04n.png"
+		});
+*/
+
+
+
+
+
+
+
+
+
