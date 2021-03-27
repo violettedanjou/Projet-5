2223b5c5c547a134c9c63b6daf17d25c21893c1f
YOYO8 <violette.danjou@hotmail.fr>
2021-03-27 13:55:23 +0100

    Suppression de tout ce qui concerne manifest.json

--- app/view/template.php
+++ app/view/template.php
@@ -13,8 +13,6 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous"> <!-- Bootstrap -->
 
-	<link rel="manifest" href="/manifest.json"> <!-- Balise du fichier manifest.json pour le lier aux autres pages-->
-
 	<link href="app/public/style.css" rel="stylesheet" /> 
 	<title><?= $title ?></title>
 </head>
