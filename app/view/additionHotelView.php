3a88ccceaf0916b36c65c43811cd288adb742ca6
YOYO8 <violette.danjou@hotmail.fr>
2021-04-06 12:35:55 +0200

    Modification form dajout dhotel

--- app/view/additionHotelView.php
+++ app/view/additionHotelView.php
@@ -26,23 +26,25 @@ ob_start(); ?>
                     <textarea id="rooms-change" name="rooms"></textarea><br/>
                     <h6>Prix des chambres</h6>
                     <textarea id="prices-change" name="prices"></textarea><br/>
+
                     <h6>Services de l'hotel</h6>
-                    <label>Piscine</label><input type="checkbox" name="swimming_pool"> 
-                    <label>Accès plage</label><input type="checkbox" name="beach_access"><br/>
-                    <label>Parking</label><input type="checkbox" name="car_park">
-                    <label>Wifi</label><input type="checkbox" name="free_wifi"><br/>
-                    <label>Restaurant</label><input type="checkbox" name="restaurant">
-                    <label>Chambres familiales</label><input type="checkbox" name="family_rooms"><br/>
-                    <label>Tévélision</label><input type="checkbox" name="television">
-                    <label>Navette aéroport</label><input type="checkbox" name="airport_shuttle"><br/>
-                    <label>Air conditionné</label><input type="checkbox" name="air_conditioner">
-                    <label>Hôtel non fumeurs</label><input type="checkbox" name="no_smokers"><br/>
-                    <label>Animaux acceptés</label><input type="checkbox" name="animals">
-                    <label>Coffre fort</label><input type="checkbox" name="strongbox"><br/>
-                    <label>Mini bar</label><input type="checkbox" name="mini_bar">
-                    <label>Bagagerie</label><input type="checkbox" name="luggage"><br/>
-                    <label>Ascenseur </label><input type="checkbox" name="elevator">
-                    <label>Sauna</label><input type="checkbox" name="sauna"><br/>
+                    <label>Piscine</label><input type="checkbox" name="services[]" value="1"> 
+                    <label>Accès plage</label><input type="checkbox" name="services[]" value="2"><br/>
+                    <label>Parking</label><input type="checkbox" name="services[]" value="3">
+                    <label>Wifi</label><input type="checkbox" name="services[]" value="4"><br/>
+                    <label>Restaurant</label><input type="checkbox" name="services[]" value="5">
+                    <label>Chambres familiales</label><input type="checkbox" name="services[]" value="6"><br/>
+                    <label>Tévélision</label><input type="checkbox" name="services[]" value="7">
+                    <label>Navette aéroport</label><input type="checkbox" name="services[]" value="8"><br/>
+                    <label>Air conditionné</label><input type="checkbox" name="services[]" value="9">
+                    <label>Hôtel non fumeurs</label><input type="checkbox" name="services[]" value="10"><br/>
+                    <label>Animaux acceptés</label><input type="checkbox" name="services[]" value="11">
+                    <label>Coffre fort</label><input type="checkbox" name="services[]" value="12"><br/>
+                    <label>Mini bar</label><input type="checkbox" name="services[]" value="13">
+                    <label>Bagagerie</label><input type="checkbox" name="services[]" value="14"><br/>
+                    <label>Ascenseur </label><input type="checkbox" name="services[]" value="15">
+                    <label>Sauna</label><input type="checkbox" name="services[]" value="16"><br/>
+
                     <input type="file" name="pictureHotel" /><br/>
                     <input type="submit" value="Enregistrer" id="button-change-activity" />
               </form>
