5c080683d8ed2e649e0c5de636a415576d44545e
YOYO8 <violette.danjou@hotmail.fr>
2021-04-19 08:59:56 +0200

    Récupérer img de profile dna sla vue : ok

--- app/view/profileView.php
+++ app/view/profileView.php
@@ -8,11 +8,14 @@
 			<div class="col">
 				<h2>PROFILE</h2>
 					<div id="block-profile">
+						<?php $profile = $profileManager->fetch() ?>
+							<img id="img-profile" src="<?= $profile['picture']?>" alt="Image de profile">
+						
 						<form id="form-profile" action="index.php?action=validProfile" method="post" enctype="multipart/form-data">
 							<input type="file" name="pictureProfile">
 							<input type="submit" name="profile" value="Modifier" id="button_profile_picture">
 						</form>
-						
+
 						<p><span id="span-profile">NOM : </span><?= $_SESSION['pseudo']?></p>
 					</div>
 				</div>
